ee5ad4adb9d5a92a453f80920ac4786985eb2e7d
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-07-10 21:08:02 -0700

    finally workinggit status

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
+function init (){
+	timeDisplay =document.createTextNode ( "");
+	document.getElementById("clock").appendChild(timeDisplay);
+}
+
 function updateClock (){
-var currentTime = new Date ();
-var currentHours = currentTime.getHours ();
-var currentMinutes = currentTime.getMinutes ();
-var currentSeconds = currentTime.getSeconds ();
+	var currentTime = new Date ();
+	var currentHours = currentTime.getHours ();
+	var currentMinutes = currentTime.getMinutes ();
+	var currentSeconds = currentTime.getSeconds ();
 
 currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes; 
 currentSeconds = (currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
+
 /* http://www.elated.com/articles/creating-a-javascript-clock/ */
-var timeOfDay = ( currentHours < 12 ) ? "AM" ; "PM";
+var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 currentHours = ( currentHours == 0 ) ? 12 : currentHours; 
 
-var currentTimeString = currentHours + ":" + currentMinutes + ":" currentSeconds + " " timeOfDay;
+var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
 document.getElementById("clock").firstChild.nodeValue = currentTimeString;
-
+}
 /* var secondHand = document.querySelector('.second-hand');
 var minuteHand = document.querySelector('.minute-hand');
 var hourHand = document.querySelector('.hour-hand');
