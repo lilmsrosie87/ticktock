4ae42ba7c0632985cf153df58df1541ccf3e837c
Roslyn Harris <rharris0113@gmail.com>
2017-11-15 22:39:59 -0800

    cleaning up code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function init (){
 	timeDisplay =document.createTextNode ( "");
 	document.getElementById("clock").appendChild(timeDisplay);
@@ -13,7 +14,7 @@ currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
 currentSeconds = (currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
 
-/* http://www.elated.com/articles/creating-a-javascript-clock/ */
+
 var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 currentHours = ( currentHours == 0 ) ? 12 : currentHours; 
@@ -22,27 +23,3 @@ var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSecon
 
 document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
-/* var secondHand = document.querySelector('.second-hand');
-var minuteHand = document.querySelector('.minute-hand');
-var hourHand = document.querySelector('.hour-hand');
-function setDate() {
-  // getting local time
-  var now = new Date();
-  var seconds = now.getSeconds();
-  var secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
-    
-  /* var minutes = now.getMinutes(); */
-/*   var minutesDegrees = ((minutes / 60) * 360) + 90;
- // minuteHand.style.transform = 'rotate(${minutesDegrees}deg)';
- console.log(minutes);
-
-  var hours = now.getHours();
-  var hoursDegrees = ((minutes / 12) * 360) + 90;
-  hourHand.style.transform = 'rotate(${hoursDegrees}deg)';
-
-
-}
-
-
-setInterval(setDate, 1000); */ 
